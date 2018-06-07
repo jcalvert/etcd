5d6a12d84ba76e5d5b36014a2029eaeeaf33250a
jocalvert <code@getbraintree.com>
2018-06-07 20:01:38 +0000

    remove dangling zap logger calls

--- mvcc/index.go
+++ mvcc/index.go
@@ -182,11 +182,7 @@ func (ti *treeIndex) RangeSince(key, end []byte, rev int64) []revision {
 
 func (ti *treeIndex) Compact(rev int64) map[revision]struct{} {
 	available := make(map[revision]struct{})
-	if ti.lg != nil {
-		ti.lg.Info("compact tree index", zap.Int64("revision", rev))
-	} else {
-		plog.Printf("store.index: compact %d", rev)
-	}
+	plog.Printf("store.index: compact %d", rev)
 	ti.Lock()
 	clone := ti.tree.Clone()
 	ti.Unlock()
@@ -200,11 +196,7 @@ func (ti *treeIndex) Compact(rev int64) map[revision]struct{} {
 		if keyi.isEmpty() {
 			item := ti.tree.Delete(keyi)
 			if item == nil {
-				if ti.lg != nil {
-					ti.lg.Panic("failed to delete during compaction")
-				} else {
-					plog.Panic("store.index: unexpected delete failure during compaction")
-				}
+				plog.Panic("store.index: unexpected delete failure during compaction")
 			}
 		}
 		ti.Unlock()
